1812852be7fedab126871e407c615b50b5b1b6a3
unknown <investor.ciwin@gmail.com>
2021-05-24 00:55:29 +0700

    Heroku Postgres

--- files/configs/db.js
+++ files/configs/db.js
@@ -1,13 +1,11 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg')
+const connectionString = 'postgres://yyhjynincomjoy:595e134dab3aeb1beb3a86757db7d1db1ab262d627fb37c27dd96c17f7fbead0@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d7egltob0tvvi3'
 
 const db = new Pool({
-   host: process.env.DB_HOST,
-   port: process.env.DB_PORT,
-   database: process.env.DB_NAME,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASS,
-   idleTimeoutMillis: 0,
-   connectionTimeoutMillis: 0,
+   connectionString,
+   ssl: {
+      rejectUnauthorized: false
+   }
 })
 
 module.exports = db
