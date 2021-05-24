39dc7b1227f39d105dc7ad04baed2101c4e08fda
unknown <investor.ciwin@gmail.com>
2021-05-25 03:33:00 +0700

    Fix nodemailer link

--- files/functions/nodemailer.js
+++ files/functions/nodemailer.js
@@ -54,7 +54,7 @@ exports.sendEmail = (token, email, name) => {
                   <img style="width: 240px; height: 80px;" src="https://image.myanimelist.net/ui/uf6p6rEk2dlZoh8DIyYQTScPXcYWVkorZzR5QFff8Dz3qY2oXvvW5x3Hrb-W5JxGHRwmguU8B08LZEEOtbv2G3WKIJ9sNYmr_hbsa72u_rSA04XHa2uGxv_npt3alTiqCO1wZi7NrJbnS1vOPJyjoZkSTvU9Syn4esm53zmf6UE"/>
                   <p style="font-size: 15px;">Halo ${name}, selamat bergabung di grup besar Telegram! Agar kamu bisa login dan mengakses akun kamu lebih lanjut, harap verifikasi akun kamu terlebih dahulu yah dengan cara menekan tombol verifikasi di bawah ini :</p>
                   <div style="margin: auto; margin-top: 40px; width: 100%;">
-                     <a class="verifyLink" href="http://localhost:5000/verify/${token}"><div class="verifyBtn">Verifikasi Akun</div></a>
+                     <a class="verifyLink" href="https://telegram-erwinsaputrasulistio.vercel.app/verify/${token}"><div class="verifyBtn">Verifikasi Akun</div></a>
                   </div>
                </div>
             </body>
